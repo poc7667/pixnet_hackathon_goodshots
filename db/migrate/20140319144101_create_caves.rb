194c38fd70ef5d1ef36f29924a1e0d0cbbb94259
poc7667 <poc7667@gmail.com>
2014-03-19 22:45:06 +0800

    Change caves to cafes, and make every column not null
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- db/migrate/20140319144101_create_caves.rb
+++ db/migrate/20140319144101_create_caves.rb
@@ -1,9 +1,9 @@
 class CreateCaves < ActiveRecord::Migration
   def change
-    create_table :caves do |t|
-      t.string :name
-      t.decimal :latitude, precision: 9, scale: 6
-      t.decimal :longitude, precision: 9, scale: 6
+    create_table :cafes do |t|
+      t.string :name, null: false
+      t.decimal :latitude, precision: 9, scale: 6, null: false
+      t.decimal :longitude, precision: 9, scale: 6, null: false
 
       t.timestamps
     end
